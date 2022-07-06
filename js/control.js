f062c492d28dc7f5fd3de5e2607d8ca52975d685
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-06 17:24:06 +0300

    Add real document

--- js/control.js
+++ js/control.js
@@ -1,12 +1,15 @@
 (() => {
   const refs = {
     controlBtn: document.querySelector("[data-control]"),
+    controlPanel: document.querySelector("[data-control-panel]"),
     logo: document.querySelector("[data-logo]"),
     backdrop: document.querySelector("[data-backdrop]"),
     frame: document.querySelector("[data-frame]"),
   };
 
   refs.controlBtn.addEventListener("click", () => {
+    refs.controlBtn.classList.toggle("is-expanded");
+    refs.controlPanel.classList.toggle("is-expanded");
     refs.logo.classList.toggle("is-expanded");
     refs.backdrop.classList.toggle("is-shown");
     refs.frame.classList.toggle("is-expanded");
